15f69ec23582ccf74e8c1c03c4f003d8da872aee
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-06-18 19:14:00 +0200

    Error check

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hpy=2r%t+63-)ctsa9dgq9u+4@y*0%f_lvacirt&0!1ny=yq9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
